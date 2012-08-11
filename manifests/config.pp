a6b21bb21bccaa1e4d7a202bbc99696260fbc768
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-08-10 22:00:05 -0700

    Add spacing

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,10 @@
 class logstash::config inherits logstash {
+
   file { "/etc/logstash.conf":
     ensure => "present",
     owner => "root",
     group => "root",
     source => "puppet:///modules/logstash/conf",
   }
+
 }
