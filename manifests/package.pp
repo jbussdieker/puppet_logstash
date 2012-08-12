6a2b257e124b218efca7e682b42a223341365dcf
Joshua Bussdieker <josh.bussdieker@moovweb.com>
2012-08-11 18:16:33 -0700

    Fix typos

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,11 +2,11 @@ class logstash::package inherits logstash {
 
   exec { "logstash_download":
     cwd => "/tmp",
-    command => "/usr/bin/wget -o /tmp/logstash-1.1.1-monolithic.jar http://semicomplete.com/files/logstash/logstash-1.1.1-monolithic.jar",
+    command => "/usr/bin/wget -O /tmp/logstash-1.1.1-monolithic.jar http://semicomplete.com/files/logstash/logstash-1.1.1-monolithic.jar",
     creates => "/tmp/logstash-1.1.1-monolithic.jar",
   }
 
-  file { "/opt/logstash-1-1-1-monolithic.jar":
+  file { "/opt/logstash-1.1.1-monolithic.jar":
     ensure => "present",
     source => "/tmp/logstash-1.1.1-monolithic.jar",
     require => Exec["logstash_download"],
